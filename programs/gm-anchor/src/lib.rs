9face422200b747ce73b6c458ee83f2122dc6023
pappas999 <harry@genesisblockchain.com.au>
2022-02-17 00:22:47 +1030

    Initial commit

---
+++ programs/gm-anchor/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+
+declare_id!("75PsQyHnFLhmF1jb4m31f2Gt35HFou3vJsaBCVAhYKAo");
+
+#[program]
+pub mod gm_anchor {
+    use super::*;
+    pub fn execute(ctx: Context<Execute>, name: String) -> ProgramResult {
+        let gm_account = &mut ctx.accounts.gm_account;
+
+        gm_account.name = name;
+        msg!("GM {}", gm_account.name);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Execute<'info> {
+    #[account(init, payer = user, space = 8 + 32)]
+    pub gm_account: Account<'info, GreetingAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct GreetingAccount {
+    pub name: String,
+}
